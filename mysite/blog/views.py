40bddb8542034c05fb06110bfe46012baf67f1d5
wdw421579475 <40289283+wdw421579475@users.noreply.github.com>
2023-02-03 02:37:19 -0800

    Update views.py
    add comment

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -64,6 +64,7 @@ def post_publish(request, pk):
 
 @login_required
 def add_comment_to_post(request, pk):
+    # create new comment object
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
